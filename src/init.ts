5081fccdb9888d4ec0b442156cbc14c24ba5a84a
naman-jain-15 <naman.jain@middleware.io>
2024-11-04 11:57:00 +0530

    Revert

--- src/init.ts
+++
@@ -1,8 +0,0 @@
-import { init as configInit } from "./config";
-import { init as profilerInit } from "./profiler";
-import { Config } from "./config";
-
-export const track = (newConfig: Partial<Config> | undefined = {}): void => {
-  const config = configInit(newConfig);
-  profilerInit(config).then((r) => {});
-};
